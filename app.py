bdf8d49f596743c011ad30e166352227d9124e2d
jeywanth <jeywanth@gmail.com>
2020-03-30 23:26:53 +0530

    C:/Program Files/Git/ has been removed

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('/index.html')
+    return render_template('index.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
